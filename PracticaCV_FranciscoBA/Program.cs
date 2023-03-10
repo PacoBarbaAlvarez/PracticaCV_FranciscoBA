493513c093153998fef79e36c25ab9d780e9a4cc
PacoBarbaAlvarez <pacobarbitaalvarez@gmail.com>
2023-03-10 13:53:39 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_FranciscoBA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_FranciscoBA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
